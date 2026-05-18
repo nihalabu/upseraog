e0204b63f2b598eeffc2fb661ea5e3e059cce09c
nihalabu <nihaalaboobacker5390@gmail.com>
2026-05-18 19:23:29 +0530

    created robots.txt and sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import sitemap from 'vite-plugin-sitemap';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      sitemap({
+        hostname: 'https://upsera.co.in',
+      }),
+    ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
@@ -16,7 +23,7 @@ export default defineConfig(({mode}) => {
       },
     },
     server: {
-      hmr: process.env.DISABLE_HMR !== 'true'
+      hmr: process.env.DISABLE_HMR !== 'true',
     },
     build: {
       chunkSizeWarningLimit: 2000,
